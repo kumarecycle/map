3693117ae98b90908d6fd255ad5d444624548458
kumarecycle <45986463+kumarecycle@users.noreply.github.com>
2018-12-19 15:16:29 +0900

    リサイクルマーク　変更　80×80

--- js/ol_style.js
+++ js/ol_style.js
@@ -6,7 +6,7 @@ var featureStyleList = {
 	'default': { color: 'rgba(153, 153, 153, 1)', img: 'image/018.png'},
 	'認可外': { color: '#0362A0', img: 'image/019.png'},
 	'幼稚園': { color: '#FF5C24', img: 'image/029.png'},
-	'スーパーマーケット': { color: '#6EE100', img: 'image/.png'}
+	'スーパーマーケット': { color: '#6EE100', img: 'image/risris8080.png'}
 };
 
 /**
